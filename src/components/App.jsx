665a5269dd51228fb02c0fedd0fb6f2e86502cc9
Kate Zaharova <15071981ktz@gmail.com>
2023-08-19 23:51:33 +0300

    add .json files and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
+import { FriendList } from "./FriendList";
+import { FriendListItem } from "./FriendListItem";
+import { Profile } from "./Profile";
+import {Statistics} from "./Statistics"
+import { TransactionHistory } from "./TransactionHistory";
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        //display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
+      <Profile />
+      <Statistics /> 
+      <FriendList />
+      <FriendListItem />
+      <TransactionHistory/>
       React homework template
     </div>
   );
